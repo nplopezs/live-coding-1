97a59ffdc3869e50c8adc9df707b201873f128a4
NatLop <nplopez_s@hotmail.com>
2023-03-21 11:14:14 -0600

    funcion

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // Escribe tu código aquí.
+//alert("Hola");
+
+function ejercicioUno(palabra,array){
+    let tamañoPalabra = palabra.length;
+    const palabras = [];
+    array.forEach(word => {
+        if(tamañoPalabra < word.length){
+            palabras.push(word);
+        }
+    });
+    console.log(palabras);
+}
+
+ejercicioUno("Hola",["Mochila","si","no","flores"]);
